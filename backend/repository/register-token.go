f3bb1f62f87b3bc9a1e3e2e2cbbc6bdaefb93dac
earlgray <earlgray283@gmail.com>
2022-06-13 17:38:33 +0900

    fix: rename

--- backend/repository/register-token.go
+++ backend/repository/register-token.go
@@ -6,15 +6,15 @@ import (
 	"cloud.google.com/go/datastore"
 )
 
-type GradeRequestToken struct {
+type RegisterToken struct {
 	UID       string    `json:"uid,omitempty"`
 	Token     string    `json:"token,omitempty"`
 	Expires   time.Time `json:"expires,omitempty"`
 	CreatedAt time.Time `json:"-"`
 }
 
-const KindGradeRequestToken = "gradeRequestToken"
+const KindRegisterToken = "registerToken"
 
-func NewGradeRequestTokenKey(token string) *datastore.Key {
-	return datastore.NameKey(KindGradeRequestToken, token, nil)
+func NewRegisterTokenKey(token string) *datastore.Key {
+	return datastore.NameKey(KindRegisterToken, token, nil)
 }
