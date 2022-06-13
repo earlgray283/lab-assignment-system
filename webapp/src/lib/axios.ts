8aa12d7f292d32b11b3b72754ec201ca005ad5fa
earlgray <earlgray283@gmail.com>
2022-06-13 11:50:34 +0900

    feat: parse date string

--- webapp/src/lib/axios.ts
+++ webapp/src/lib/axios.ts
@@ -3,6 +3,26 @@ import axios, { AxiosResponse } from 'axios';
 export const http = axios.create({
   baseURL: `${import.meta.env.VITE_BACKEND_HOST}`,
   withCredentials: true,
+  transformResponse: (data) => {
+    if (typeof data === 'string') {
+      try {
+        return JSON.parse(data, (k: string, val: unknown) => {
+          if (typeof val === 'string') {
+            console.log(val);
+            const date = new Date(val);
+            if (Number.isNaN(date.getDate())) {
+              return val;
+            }
+            return date;
+          }
+          return val;
+        });
+      } catch (e) {
+        return data;
+      }
+    }
+    return data;
+  },
 });
 
 export async function postJson<
