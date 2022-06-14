854575e3c5cb3f03d8262a8115d78e9420478c5d
earlgray <earlgray283@gmail.com>
2022-06-14 20:09:17 +0900

    refactor: 単純な fetch に

--- backend/server/labs.go
+++ backend/server/labs.go
@@ -20,37 +20,13 @@ func (srv *Server) LabsRouter() {
 func (srv *Server) HandleGetLabs() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		ctx := c.Request.Context()
-		labIds, _ := c.GetQueryArray("labId")
-		keys := make([]*datastore.Key, 0)
-		if len(labIds) == 0 {
-			keys2, err := srv.dc.GetAll(ctx, datastore.NewQuery(repository.KindLab).KeysOnly(), nil)
-			if err != nil {
-				srv.logger.Println("check", err)
-				c.AbortWithStatus(http.StatusInternalServerError)
-			}
-			keys = keys2
-		} else {
-			for _, labId := range labIds {
-				keys = append(keys, repository.NewLabKey(labId))
-			}
-		}
 
-		repoLabs := make([]repository.Lab, len(keys))
-		if err := srv.dc.GetMulti(ctx, keys, repoLabs); err != nil {
-			srv.logger.Println(err)
-			if merr, ok := err.(datastore.MultiError); !ok {
-				for _, err := range merr {
-					if err == datastore.ErrNoSuchEntity {
-						AbortWithErrorJSON(c, NewError(http.StatusNotFound, "no such lab"))
-						return
-					}
-				}
-			}
+		repoLabs := make([]*repository.Lab, 0, 100)
+		if _, err := srv.dc.GetAll(ctx, datastore.NewQuery(repository.KindLab), repoLabs); err != nil {
 			c.AbortWithStatus(http.StatusInternalServerError)
-			return
 		}
 
-		labs := make([]*models.Lab, len(keys))
+		labs := make([]*models.Lab, len(repoLabs))
 		for i, repoLab := range repoLabs {
 			labs[i] = &models.Lab{
 				ID:       repoLab.ID,
