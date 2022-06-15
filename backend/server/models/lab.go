454a4ebf20278e1af27d3290b221b61f5479e6b0
earlgray <earlgray283@gmail.com>
2022-06-15 17:42:18 +0900

    fix: remove omitempty attr

--- backend/server/models/lab.go
+++ backend/server/models/lab.go
@@ -8,7 +8,7 @@ type Lab struct {
 	ID           string `json:"id,omitempty"`
 	Name         string `json:"name,omitempty"`
 	Capacity     int    `json:"capacity,omitempty"`
-	FirstChoice  int    `json:"firstChoice,omitempty"`
-	SecondChoice int    `json:"secondChoice,omitempty"`
-	ThirdChoice  int    `json:"thirdChoice,omitempty"`
+	FirstChoice  int    `json:"firstChoice"`
+	SecondChoice int    `json:"secondChoice"`
+	ThirdChoice  int    `json:"thirdChoice"`
 }
