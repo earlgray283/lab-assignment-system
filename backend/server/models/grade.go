3e44101168a69896b35fd62fb7c0eb302301077c
earlgray <earlgray283@gmail.com>
2022-06-16 01:12:07 +0900

    fix: grade payload

--- backend/server/models/grade.go
+++ backend/server/models/grade.go
@@ -1,7 +1,6 @@
 package models
 
 type SubjectGrade struct {
-	SubjectName string  `json:"subjectName,omitempty"` // 科目名
 	UnitNum     int     `json:"unitNum,omitempty"`     // 単位
 	Point       float64 `json:"point,omitempty"`       // 点数
 	Gp          float64 `json:"gp,omitempty"`          // GP
@@ -10,6 +9,6 @@ type SubjectGrade struct {
 
 type Grade struct {
 	Grades        []SubjectGrade `json:"grades,omitempty"`
-	StudentName   string
-	StudentNumber string
+	StudentName   string         `json:"studentName,omitempty"`
+	StudentNumber string         `json:"studentNumber,omitempty"`
 }
