a312232286889f77702b3eb105644b8368f67c85
earlgray <earlgray283@gmail.com>
2022-06-16 01:35:57 +0900

    feat: コピーボタン

--- webapp/src/components/CopyBox.tsx
+++ webapp/src/components/CopyBox.tsx
@@ -2,6 +2,7 @@ import { IconButton, Tooltip } from '@mui/material';
 import React, { useState } from 'react';
 import './copy-box.css';
 import ContentPasteIcon from '@mui/icons-material/ContentPaste';
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 function CopyBox(props: {
   children: React.ReactNode;
@@ -11,17 +12,23 @@ function CopyBox(props: {
   return (
     <div className='copy-box'>
       {props.children}
-      <Tooltip
-        arrow
-        open={openTooltip}
-        onClose={() => setOpenTooltip(false)}
-        placement='top'
-        title='Copied'
+
+      <CopyToClipboard
+        text={props.copyText}
+        onCopy={() => setOpenTooltip(true)}
       >
-        <IconButton size='small' sx={{ alignSelf: 'right' }}>
-          <ContentPasteIcon />
-        </IconButton>
-      </Tooltip>
+        <Tooltip
+          arrow
+          open={openTooltip}
+          onClose={() => setOpenTooltip(false)}
+          placement='top'
+          title='Copied'
+        >
+          <IconButton size='small' sx={{ alignSelf: 'right' }}>
+            <ContentPasteIcon />
+          </IconButton>
+        </Tooltip>
+      </CopyToClipboard>
     </div>
   );
 }
