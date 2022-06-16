4c99978ab63985df9fefee97ca3387d05fc83b11
earlgray <earlgray283@gmail.com>
2022-06-16 15:06:28 +0900

    fix: get all labs

--- backend/server/labs.go
+++ backend/server/labs.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"strings"
 
+	"cloud.google.com/go/datastore"
 	"github.com/gin-gonic/gin"
 )
 
@@ -22,7 +23,6 @@ func (srv *Server) HandleGetAllLabs() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		ctx := c.Request.Context()
 
-		var repoLabs []*repository.Lab
 		labIds := strings.Split(c.Query("labIds"), "+")
 		var optFields []string
 		if optFieldsText, ok := c.GetQuery("optFields"); ok {
@@ -35,15 +35,26 @@ func (srv *Server) HandleGetAllLabs() gin.HandlerFunc {
 				return
 			}
 		}
-		repoLabs, ok, err := repository.FetchAllLabs(ctx, srv.dc, labIds)
-		if err != nil {
-			srv.logger.Println(err)
-			c.AbortWithStatus(http.StatusInternalServerError)
-			return
-		}
-		if !ok {
-			AbortWithErrorJSON(c, NewError(http.StatusNotFound, "no such lab"))
-			return
+
+		var repoLabs []*repository.Lab
+		if len(labIds) == 0 {
+			if _, err := srv.dc.GetAll(ctx, datastore.NewQuery(repository.KindLab), &repoLabs); err != nil {
+				srv.logger.Println(err)
+				c.AbortWithStatus(http.StatusInternalServerError)
+				return
+			}
+		} else {
+			repoLabs2, ok, err := repository.FetchAllLabs(ctx, srv.dc, labIds)
+			if err != nil {
+				srv.logger.Println(err)
+				c.AbortWithStatus(http.StatusInternalServerError)
+				return
+			}
+			if !ok {
+				AbortWithErrorJSON(c, NewError(http.StatusNotFound, "no such lab"))
+				return
+			}
+			repoLabs = repoLabs2
 		}
 
 		labs := make([]*models.Lab, len(repoLabs))
