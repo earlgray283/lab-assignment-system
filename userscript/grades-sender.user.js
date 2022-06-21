c7bbf4c0e4a7d227513673266b37c6e467bb4f34
earlgray <earlgray283@gmail.com>
2022-06-21 19:38:42 +0900

    fix: version 0.1 -> 0.1.1

--- userscript/grades-sender.user.js
+++ userscript/grades-sender.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         grades-sender
 // @namespace    https://lab-assignment-system-project.web.app/
-// @version      0.1
+// @version      0.1.1
 // @description  成績情報を取得し、lab-assignment-system に送信する。
 // @author       earlgray
 // @match        https://gakujo.shizuoka.ac.jp/kyoumu/seisekiSearchStudentInit.do*
