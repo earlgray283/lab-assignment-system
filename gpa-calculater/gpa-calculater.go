0f7d8d29aa64c99fd04962ce3c80c8416657ac7f
earlgray <earlgray283@gmail.com>
2022-07-11 15:37:42 +0900

    fix: 学籍番号昇順に

--- gpa-calculater/gpa-calculater.go
+++ gpa-calculater/gpa-calculater.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -32,12 +33,20 @@ type Pair[T, U any] struct {
 	Second U
 }
 
+func makeSliceFromMap[K comparable, V any](m map[K]V) []*Pair[K, V] {
+	s := make([]*Pair[K, V], 0, len(m))
+	for k, v := range m {
+		s = append(s, &Pair[K, V]{k, v})
+	}
+	return s
+}
+
 func main() {
 	gradeFile, err := os.Open("grade.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
-	grades := []*GradeCsv{}
+	gradeCsvRows := []*GradeCsv{}
 	r := csv.NewReader(gradeFile)
 	_, _ = r.Read() // ラベルを読み飛ばす
 	for {
@@ -53,7 +62,7 @@ func main() {
 		point, _ := strconv.ParseFloat(record[7], 64)
 		credit, _ := strconv.Atoi(record[11])
 		schoolGrade, _ := strconv.Atoi(record[14])
-		grades = append(grades, &GradeCsv{
+		gradeCsvRows = append(gradeCsvRows, &GradeCsv{
 			Department:     record[0],
 			Course:         record[1],
 			StudentNumber:  record[2],
@@ -75,7 +84,7 @@ func main() {
 	gradeFile.Close()
 
 	gradeMap := map[string]*Pair[float64, int]{}
-	for _, grade := range grades {
+	for _, grade := range gradeCsvRows {
 		if _, ok := gradeMap[grade.StudentNumber]; !ok {
 			gradeMap[grade.StudentNumber] = &Pair[float64, int]{}
 		}
@@ -90,15 +99,20 @@ func main() {
 		gradeMap[grade.StudentNumber].Second += grade.Credit
 	}
 
+	grades := makeSliceFromMap(gradeMap)
+	sort.Slice(grades, func(i, j int) bool {
+		return grades[i].First < grades[j].First
+	})
+
 	gpaCsvFile, err := os.Create("gpa.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer gpaCsvFile.Close()
 	w := csv.NewWriter(gpaCsvFile)
-	for name, grade := range gradeMap {
-		gpa := grade.First / float64(grade.Second)
-		cols := []string{name, strconv.FormatFloat(gpa, 'f', -1, 64)}
+	for _, grade := range grades {
+		gpa := grade.Second.First / float64(grade.Second.Second)
+		cols := []string{grade.First, strconv.FormatFloat(gpa, 'f', -1, 64)}
 		if err := w.Write(cols); err != nil {
 			log.Fatal(err)
 		}
